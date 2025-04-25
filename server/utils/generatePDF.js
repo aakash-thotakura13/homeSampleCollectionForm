35de2098c80180f955d4b916286cfcda68cb22e9
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-04-25 17:09:20 +0530

    added cloudinary config file and updated the code in generatePDF.js file

--- server/utils/generatePDF.js
+++ server/utils/generatePDF.js
@@ -1,24 +1,37 @@
+const fs = require("fs");
+const path = require("path");
 const puppeteer = require("puppeteer");
 const ejs = require("ejs");
-const path = require("path");
-const fs = require("fs");
 
-const generatePDF = async (data, filenName) => {
-  
-  const templatePath = path.join(__dirname, "../templates/formTemplate.ejs");
+const cloudinary = require("./cloudinary");
+
+const generatePDF = async (data, fileName) => {
+  const templatePath = path.join(__dirname, "../templates/pdfTemplate.ejs");
   const html = await ejs.renderFile(templatePath, { data });
+
+  const pdfsDir = path.join(__dirname, "../pdfs");
+
+  if (!fs.existsSync(pdfsDir)) {
+    fs.mkdirSync(pdfsDir);
+  }
+
+  const pdfPath = path.join(pdfsDir, `${fileName}.pdf`);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   await page.setContent(html, { waitUntil: "domcontentloaded" });
 
-  const pdfPath = path.join(__dirname, `../pdfs/${filenName}.pdf`);
   await page.pdf({ path: pdfPath, format: "A4" });
-
   await browser.close();
 
-  return pdfPath;
+  const result = await cloudinary.uploader.upload(pdfPath, {
+    resource_type: "raw",
+    folder: "hsc_pdfs",
+    public_id: fileName,
+    overwrite: true,
+  });
+
+  return result.secure_url;
 };
 
 module.exports = generatePDF;
