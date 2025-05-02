2eed074f87d4c4607be1e1599561fa7bdbb6cd4b
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-05-02 20:45:49 +0530

    made minor changes

--- server/utils/generatePDF.js
+++ server/utils/generatePDF.js
@@ -1,5 +1,5 @@
-const chromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 const ejs = require("ejs");
 const path = require("path");
 const fs = require("fs");
